cae2cf9053b868f085be10ce544833ae68d190a7
ndowah <70651812+NDOWAH@users.noreply.github.com>
2022-08-16 17:04:00 +0100

    add another function to put watermark in a pdf document

--- socket_programming/extract.py
+++ socket_programming/extract.py
@@ -1,4 +1,4 @@
-from PyPDF2 import PdfFileReader
+from PyPDF2 import PdfFileReader, PdfFileWriter
 
 
 def extract_infomation(document_path):
@@ -14,3 +14,15 @@ def extract_infomation(document_path):
     print(doc)
     return info
 
+def put_watermark(file_path, watermark,output):
+    #output here is the destination file we which to write our result
+    pdf_reader = PdfFileReader(file_path)
+    pdf_writer = PdfFileWriter()
+    watermark_obj = PdfFileReader(watermark)
+    watermark_page = watermark_obj.getpage(0)
+    for page in range(pdf_reader.getPageNumber()):
+        page = pdf_reader.getPage(page)
+        page.mergePage(watermark_page)
+        pdf_writer.addPage(page)
+        with open(output) as f:
+            pdf_writer.write(f)
